0cd0e83c9499f0a34d19527209c2b392cad4b6d6
danbiilee <danbi.db@gmail.com>
2022-04-14 11:52:06 +0900

    chore: 웹팩 설정 파일 중복 제거
    - 공통 설정들이 모든 패키지에 중복해서 존재 -> 공통 설정 하나 바뀌면 모든 파일 수정해줘야 함
    - 공통 설정은 워크스페이스 루트에 webpack.common.js으로 분리
    - 각 패키지에서 webpack-merge로 합침

--- react-react-shared/remotes/app2/webpack.config.js
+++ react-react-shared/remotes/app2/webpack.config.js
@@ -1,12 +1,7 @@
-const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('../../webpack.common');
 const { ModuleFederationPlugin } = webpack.container;
 const { MFLiveReloadPlugin } = require('@module-federation/fmr');
 const deps = require('./package.json').dependencies;
@@ -40,121 +35,20 @@ const mfConfig = {
 };
 
 // Webpack's Configuration
-const config = {
-  entry: './src/index.tsx',
-  mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'eval-cheap-module-source-map' : false,
+const config = merge(common(port), {
   devServer: {
-    port: parseInt(port),
-    hot: true,
-    static: { directory: path.join(__dirname, 'public') },
-    historyApiFallback: true,
-    compress: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[contenthash:8][ext][query]',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    targets: { browsers: ['last 2 chrome versions'] },
-                    debug: isDevelopment,
-                  },
-                ],
-                '@babel/preset-react',
-                '@babel/preset-typescript',
-              ],
-              plugins: [
-                isDevelopment && require.resolve('react-refresh/babel'),
-              ].filter(Boolean),
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(p|s)?css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(svg|png|jpe?g|gif|ico)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /.(woff2?|eot|(o|t)tf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json'],
-    alias: {},
-  },
   plugins: [
     new ModuleFederationPlugin(mfConfig),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].[contenthash:8].css',
-    }),
     isDevelopment &&
       new MFLiveReloadPlugin({
         port: parseInt(port),
         container: app2Name,
       }),
-    isDevelopment && new ReactRefreshWebpackPlugin(),
-    !isDevelopment && new webpack.LoaderOptionsPlugin({ minimize: true }),
-    !isDevelopment &&
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: `report_${Date.now()}.html`,
-        openAnalyzer: false,
-      }),
   ].filter(Boolean),
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-      new CssMinimizerPlugin({
-        minimizerOptions: {
-          preset: [
-            'default',
-            {
-              discardComments: { removeAll: true },
-            },
-          ],
-        },
-      }),
-    ],
-    splitChunks: {},
-  },
-};
+});
 
 module.exports = config;
---
+++ react-react-shared/webpack.common.js
@@ -0,0 +1,124 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+// Webpack's Configuration
+const config = port => {
+  return {
+    entry: './src/index.tsx',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-cheap-module-source-map' : false,
+    devServer: {
+      port: parseInt(port),
+      hot: true,
+      static: { directory: path.join(__dirname, 'public') },
+      historyApiFallback: true,
+      compress: true,
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isDevelopment ? '[name].js' : '[name].[contenthash:8].js',
+      assetModuleFilename: 'assets/[contenthash:8][ext][query]',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      targets: { browsers: ['last 2 chrome versions'] },
+                      debug: isDevelopment,
+                    },
+                  ],
+                  '@babel/preset-react',
+                  '@babel/preset-typescript',
+                ],
+                plugins: [
+                  isDevelopment && require.resolve('react-refresh/babel'),
+                ].filter(Boolean),
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(p|s)?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(svg|png|jpe?g|gif|ico)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /.(woff2?|eot|(o|t)tf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.json'],
+      alias: {},
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[contenthash:8].css',
+      }),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+      !isDevelopment && new webpack.LoaderOptionsPlugin({ minimize: true }),
+      !isDevelopment &&
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          reportFilename: `report_${Date.now()}.html`,
+          openAnalyzer: false,
+        }),
+    ].filter(Boolean),
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          terserOptions: {
+            format: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        }),
+        new CssMinimizerPlugin({
+          minimizerOptions: {
+            preset: [
+              'default',
+              {
+                discardComments: { removeAll: true },
+              },
+            ],
+          },
+        }),
+      ],
+      splitChunks: {},
+    },
+  };
+};
+
+module.exports = config;
